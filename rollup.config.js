a41e9a9b41b88f647c4e5ba065119249754e14b5
Derrick Reimer <derrickreimer@gmail.com>
2019-09-23 15:39:37 -0500

    Use statickit/core

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,12 @@
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 const plugins = [
+  nodeResolve(),
+  commonjs({
+    include: 'node_modules/**'
+  }),
   babel({
     exclude: 'node_modules/**'
   })
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import StaticKit from '@statickit/core';
 
 function ValidationError(props) {
   const { prefix, field, errors, ...attrs } = props;
@@ -18,51 +19,61 @@ function ValidationError(props) {
   );
 }
 
-function useForm(id, apiUrl) {
+function useForm(props) {
   const [submitting, setSubmitting] = useState(false);
   const [succeeded, setSucceeded] = useState(false);
   const [errors, setErrors] = useState([]);
+  const [client, setClient] = useState(undefined);
 
-  const submit = e => {
-    const form = e.target;
-    const url =
-      (apiUrl || 'https://api.statickit.com') +
-      '/j/forms/' +
-      id +
-      '/submissions';
+  useEffect(() => {
+    const client = StaticKit();
+    setClient(client);
+  }, []);
+
+  const id = typeof props === 'object' ? props.id : props;
+  if (!id) throw new Error('You must define an `id` property');
+
+  const endpoint = props.endpoint || 'https://api.statickit.com';
+  const debug = !!props.debug;
+
+  const submit = event => {
+    const form = event.target;
+
+    if (form.tagName != 'FORM')
+      throw new Error('submit was triggered for a non-form element');
+
+    const formData = new FormData(form);
+
+    const sendRequest = async () => {
+      try {
+        const result = await client.submitForm({
+          id: id,
+          endpoint: endpoint,
+          data: formData
+        });
+
+        if (result.response.status == 200) {
+          if (debug) console.log(id, 'Submitted', result);
+          setSucceeded(true);
+          setErrors([]);
+        } else {
+          const errors = result.body.errors;
+          if (debug) console.log(id, 'Validation error', result);
+          setSucceeded(false);
+          setErrors(errors);
+        }
+      } catch (e) {
+        if (debug) console.log(id, 'Unexpected error', e);
+        setSucceeded(false);
+      } finally {
+        setSubmitting(false);
+      }
+    };
 
     e.preventDefault();
     setSubmitting(true);
-
-    fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      body: new FormData(form)
-    })
-      .then(response => {
-        response.json().then(data => {
-          switch (response.status) {
-            case 200:
-              setSucceeded(true);
-              break;
-
-            case 422:
-              setSucceeded(false);
-              setErrors(data.errors);
-              break;
-
-            default:
-              setSucceeded(false);
-              break;
-          }
-        });
-      })
-      .catch(error => {
-        setSucceeded(false);
-      })
-      .finally(() => {
-        setSubmitting(false);
-      });
+    sendRequest();
+    return true;
   };
 
   return [{ submitting, succeeded, errors }, submit];
