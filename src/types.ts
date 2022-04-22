fc4321664c525c9b5d9b7803f5f5a0f9ad677d49
Kotaro Sugawara <kotarella1110@gmail.com>
2022-04-22 12:43:56 +0900

    refactor: add FieldErrorCode type to enhance ErrorPayload type safely

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,11 @@
+type FieldErrorCode =
+  | 'REQUIRED_FIELD_MISSING'
+  | 'REQUIRED_FIELD_EMPTY'
+  | 'TYPE_EMAIL'
+  | 'TYPE_NUMERIC'
+  | 'TYPE_TEXT';
 export interface ErrorPayload {
   field?: string;
-  code: string | null;
+  code: FieldErrorCode | null;
   message: string;
 }
