0c8faaab104b1c536308b941674c156f437d364b
smakosh <ismai23l@hotmail.com>
2022-07-03 00:37:40 +0200

    fix: use @vitejs/plugin-react

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
-import ReactPlugin from 'vite-preset-react';
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    ReactPlugin({
-      injectReact: false
-    })
+    react(),
   ]
 });
