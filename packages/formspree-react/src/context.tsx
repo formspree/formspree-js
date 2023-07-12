220769ac6d5e05b579b90b28aab9b1188a854445
Thanik Bhongbhibhat <email@thanik.me>
2023-07-12 16:06:47 +0700

    update tests: context

--- packages/formspree-react/src/context.tsx
+++ packages/formspree-react/src/context.tsx
@@ -1,6 +1,6 @@
 import { createClient, getDefaultClient, type Client } from '@formspree/core';
 import type { Stripe } from '@stripe/stripe-js';
-import { loadStripe } from '@stripe/stripe-js/pure.js';
+import { loadStripe } from '@stripe/stripe-js/pure';
 import React, { useContext, useEffect, useState, type ReactNode } from 'react';
 
 export type FromspreeContextType = {
@@ -30,16 +30,24 @@ const getStripe = (stripeKey: string) => {
  * FormspreeProvider creates Formspree Client based on the given props
  * and makes the client available through via context.
  */
-export const FormspreeProvider = (props: FormspreeProviderProps) => {
+export function FormspreeProvider(props: FormspreeProviderProps) {
   const { children, project, stripePK } = props;
 
   const [client, setClient] = useState<Client>(createClient({ project }));
   const [stripe, setStripe] = useState<Stripe | undefined>(undefined);
 
   useEffect(() => {
+    let isMounted = true;
     if (stripePK) {
-      getStripe(stripePK).then((stripe) => stripe && setStripe(stripe));
+      getStripe(stripePK).then((stripe) => {
+        if (stripe && isMounted) {
+          setStripe(stripe);
+        }
+      });
     }
+    return () => {
+      isMounted = false;
+    };
   }, [stripePK]);
 
   useEffect(() => {
@@ -51,7 +59,7 @@ export const FormspreeProvider = (props: FormspreeProviderProps) => {
       {children}
     </FormspreeContext.Provider>
   );
-};
+}
 
 export function useFormspree(): FromspreeContextType {
   return useContext(FormspreeContext) ?? { client: getDefaultClient() };
---
+++ packages/formspree-react/test/context.test.tsx
@@ -0,0 +1,64 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import React from 'react';
+import { FormspreeProvider, useFormspree } from '../src';
+
+jest.mock('@stripe/stripe-js/pure');
+
+import type { Stripe } from '@stripe/stripe-js';
+import { loadStripe } from '@stripe/stripe-js/pure';
+
+describe('FormspreeProvider', () => {
+  describe('default', () => {
+    it('creates a client available via context', () => {
+      const { result } = renderHook(useFormspree, {
+        wrapper: ({ children }) => (
+          <FormspreeProvider>{children}</FormspreeProvider>
+        ),
+      });
+      const { client } = result.current;
+      expect(client).toBeTruthy();
+      expect(client.project).toBeUndefined();
+      expect(client.stripe).toBeUndefined();
+    });
+  });
+
+  describe('with project', () => {
+    it('creates a client available via context', () => {
+      const project = 'test-project-id';
+      const { result } = renderHook(useFormspree, {
+        wrapper: ({ children }) => (
+          <FormspreeProvider project={project}>{children}</FormspreeProvider>
+        ),
+      });
+      const { client } = result.current;
+      expect(client).toBeTruthy();
+      expect(client.project).toBe(project);
+      expect(client.stripe).toBeUndefined();
+    });
+  });
+
+  describe('with Stripe', () => {
+    it('creates a client available via context', async () => {
+      const stripe = {} as Stripe;
+      const mockedLoadStripe = loadStripe as jest.MockedFn<typeof loadStripe>;
+      mockedLoadStripe.mockResolvedValue(stripe);
+
+      const stripePK = 'test-stripe-public-key';
+      const { result } = renderHook(useFormspree, {
+        wrapper: ({ children }) => (
+          <FormspreeProvider stripePK={stripePK}>{children}</FormspreeProvider>
+        ),
+      });
+
+      const { client } = result.current;
+      expect(client).toBeTruthy();
+      expect(client.project).toBeUndefined();
+
+      await waitFor(() => {
+        // need to grab the updated client from result.current
+        const { client } = result.current;
+        expect(client.stripe).not.toBeUndefined();
+      });
+    });
+  });
+});
