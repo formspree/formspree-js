488a94f3a530e66d424b81ce498f46f37fe040f7
Thanik Bhongbhibhat <mail@thanik.me>
2025-03-02 21:53:09 +0700

    revert react-copy-to-clipboard changes

--- examples/cra-demo/src/CardExample.tsx
+++ examples/cra-demo/src/CardExample.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import copy from 'copy-to-clipboard';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 type CardExampleProps = {
   title: string;
@@ -9,30 +9,31 @@ type CardExampleProps = {
 const CardExample = ({ title, cardNumber }: CardExampleProps) => {
   const [isCopied, setCopied] = useState(false);
 
-  function handleCopyButtonClick(): void {
-    setCopied(true);
-    copy(cardNumber);
-    setTimeout(() => {
-      setCopied(false);
-    }, 1000);
-  }
-
   return (
     <li>
       {title}
-      <svg
-        aria-hidden="true"
-        onClick={handleCopyButtonClick}
-        height="14"
-        width="14"
-        viewBox="0 0 16 16"
-        xmlns="http://www.w3.org/2000/svg"
+      <CopyToClipboard
+        text={cardNumber}
+        onCopy={() => {
+          setCopied(true);
+          setTimeout(() => {
+            setCopied(false);
+          }, 1000);
+        }}
       >
-        <path
-          d="M7 5h2a3 3 0 0 0 3-3 2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2 3 3 0 0 0 3 3zM6 2a2 2 0 1 1 4 0 1 1 0 0 1-1 1H7a1 1 0 0 1-1-1z"
-          fillRule="evenodd"
-        />
-      </svg>
+        <svg
+          aria-hidden="true"
+          height="14"
+          width="14"
+          viewBox="0 0 16 16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7 5h2a3 3 0 0 0 3-3 2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2 3 3 0 0 0 3 3zM6 2a2 2 0 1 1 4 0 1 1 0 0 1-1 1H7a1 1 0 0 1-1-1z"
+            fillRule="evenodd"
+          />
+        </svg>
+      </CopyToClipboard>
       {isCopied && <div className="alert">Copied!</div>}
     </li>
   );
